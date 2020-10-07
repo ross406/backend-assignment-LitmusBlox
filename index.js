4cc39df0c6864a7ea1b7dfd37f3eb1df87598623
Roshan sharma <roshansharma406@gmail.com>
2020-10-07 22:07:24 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,24 @@
+const csv = require('csv-parser');
+const fs = require('fs');
+const results = [];
+const result = [];
+
+fs.createReadStream('JavaProject.csv')
+  .pipe(csv({}))
+  .on('data', (data) => results.push(data))
+  .on('end', () => {
+    for (let i = 0; i < results.length; i++) {
+      let obj = {};
+      obj.STATUS = results[i].STATUS;
+      obj.PRIORITY = results[i].PRIORITY;
+      obj.DEADLINE = results[i].DEADLINE;
+      obj['FIXED COST'] = results[i]['FIXED COST'];
+      obj['ACTUAL HRS'] = results[i]['ACTUAL HRS'];
+      obj.TOTAL =
+        parseInt(results[i]['FIXED COST'].slice(1)) +
+        parseInt(results[i]['ACTUAL HRS']);
+
+      result.push(obj);
+    }
+    console.log(result);
+  });
